943ebda7771674eb8221508217111d837f3639a2
Miguel-Sol <mi7215so-s@student.lu.se>
2022-12-16 17:56:12 +0100

    Added Update function on tableView
    Made some changes in some classes, in order to make the TableView update function work.
    
    What needs to be added: more try-catch and check how to retrive a value into a comboBox. And fix so that teacherLastName show up in tableView

--- src/application/Teacher.java
+++ src/application/Teacher.java
@@ -2,87 +2,101 @@ package application;
 
 import java.util.ArrayList;
 
-
 public class Teacher {
-	
+
 	private int employeeID;
 	private String teacherName;
 	private String teacherLastName;
 	private String teacherAddress;
-	private double hourlySalary; //may not be negative
-	private String teacherTitle;	
-	
+	private double hourlySalary; // may not be negative
+	private String teacherTitle;
+
 	private Department department;
-	
+
 	private ArrayList<Course> teaching = new ArrayList<>();
-	private ArrayList<Course> responsible = new ArrayList<>();	
-	
-	
-	
-	//Constructor
-	public Teacher(int employeeID, String teacherName, String teacherLastname, String teacherTitle, String teacherAddress, double hourlySalary) {
+	private ArrayList<Course> responsible = new ArrayList<>();
+
+	// Constructor
+	public Teacher(int employeeID, String teacherName, String teacherLastName, String teacherTitle,
+			String teacherAddress, double hourlySalary) {
 		this.employeeID = employeeID;
 		this.teacherName = teacherName;
 		this.teacherTitle = teacherTitle;
 		this.teacherAddress = teacherAddress;
 		this.hourlySalary = hourlySalary;
 	}
-	//Getters and setters
-	public int getEmployeeID() {
-		return employeeID;
-	}
-	public void setEmployeeID(int employeeID) {
-		this.employeeID = employeeID;
-	}
-	public String getTeacherName() {
-		return teacherName;
-	}
-	public void setTeacherName(String teacherName) {
-		this.teacherName = teacherName;
-	}
-	public String getTeacherTitle() {
-		return teacherTitle;
-	}
-	public void setTeacherTitle(String teacherTitle) {
-		this.teacherTitle = teacherTitle;
-	}
-	public String getTeacherAddress() {
-		return teacherAddress;
-	}
-	public void setTeacherAddress(String teacherAddress) {
-		this.teacherAddress = teacherAddress;
-	}
-	public double getHourlySalary() {
-		return hourlySalary;
-	}
-	public void setHourlySalary(double hourlySalary) {
-		this.hourlySalary = hourlySalary;
-	
-	}
+	// Getters and setters
+
 	public Department getDepartment() {
 		return department;
 	}
-	public void setDepartment(Department department) {
-		this.department = department;
+
+	public int getEmployeeID() {
+		return employeeID;
 	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public String getTeacherName() {
+		return teacherName;
+	}
+
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+
 	public String getTeacherLastName() {
 		return teacherLastName;
 	}
+
 	public void setTeacherLastName(String teacherLastName) {
 		this.teacherLastName = teacherLastName;
 	}
+
+	public String getTeacherAddress() {
+		return teacherAddress;
+	}
+
+	public void setTeacherAddress(String teacherAddress) {
+		this.teacherAddress = teacherAddress;
+	}
+
+	public double getHourlySalary() {
+		return hourlySalary;
+	}
+
+	public void setHourlySalary(double hourlySalary) {
+		this.hourlySalary = hourlySalary;
+	}
+
+	public String getTeacherTitle() {
+		return teacherTitle;
+	}
+
+	public void setTeacherTitle(String teacherTitle) {
+		this.teacherTitle = teacherTitle;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
 	public ArrayList<Course> getTeaching() {
 		return teaching;
 	}
+
 	public void setTeaching(ArrayList<Course> teaching) {
 		this.teaching = teaching;
 	}
+
 	public ArrayList<Course> getResponsible() {
 		return responsible;
 	}
+
 	public void setResponsible(ArrayList<Course> responsible) {
 		this.responsible = responsible;
 	}
 
-	}
-
+}
